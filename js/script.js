68fef1eb96b22772176df01ac9e5bda989fb54ab
Vianney Baskinas <vianbask07@yahoo.com>
2023-03-14 21:59:18 -0700

    init commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const $ = (id) => document.getElementById(id)
+let id = $('id')
+let name = $('name')
+let ext = $('ext')
+let email = $('email')
+let department = $('department')
+let displayResult = document.getElementById('empForm')
+displayResult.addEventListener('submit',(e) =>{
+    e.preventDefault();
+    console.log(`ID: ${id.value}`)
+    console.log(`Name: ${name.value}`)
+    console.log(`Extension: ${ext.value}`)
+    console.log(`Email: ${email.value}`)
+    console.log(`Department: ${department.value}`)
+})
